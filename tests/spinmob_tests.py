8d9c9abdda360ed9f35678fd3637abaa45b88cc8
Jack Sankey <jack.sankey@gmail.com>
2025-12-31 18:21:56 -0500

    Spinmob v3.12.0 passes Win11

--- tests/spinmob_tests.py
+++ tests/spinmob_tests.py
@@ -3,17 +3,17 @@ import shutil   as _sh
 import os       as _os
 import sys      as _sys
 
-# from test__plot      import *
-# from test__databox   import *
-# from test__fitter    import *
-# from test__functions import *
-# from test__dialogs   import *
-# from test__egg       import *
-
-import spinmob
-runfile(spinmob.__path__[0] + '/tests/test__egg.py')
+from test__plot      import *
+from test__databox   import *
+from test__fitter    import *
+from test__functions import *
+from test__dialogs   import *
+from test__egg       import *
 
 if _os.path.exists('egg_settings'): _sh.rmtree('egg_settings')
 
-_sys.argv = _sys.argv[0:1]
-_ut.main(failfast=True)
+_ut.main(
+        argv=['first-arg-is-ignored'], 
+        exit=False, 
+        buffer=False, 
+    )
