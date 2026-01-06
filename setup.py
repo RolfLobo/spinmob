d5387fe7e6e7c3907ee16331ffaaf0814073f2eb
Jack Sankey <jack.sankey@gmail.com>
2026-01-06 14:30:37 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-__version__ = '3.13.5' # Keep this on the first line so it's easy for __init__.py to grab.
+__version__ = '3.13.7' # Keep this on the first line so it's easy for __init__.py to grab.
 # We now match the first two spinmob version numbers to the python version 
 # against which it is tested. The last digit is incremented for spinmob changes.
 
